52c210e1fecc6bd8e41dc4fbc0bdf515fb541c5a
M00St4r <64206620+M00St4r@users.noreply.github.com>
2025-06-25 12:24:21 +0200

    Add CharacterController

---
+++ DragonFeast/Assets/rbBasedController.cs
@@ -0,0 +1,56 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class rbBasedController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+
+    public Transform groundCheck;
+    public LayerMask GroundMask;
+    public float groundDistance;
+    public float groundDrag;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void FixedUpdate()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * x + transform.forward * z;
+        if (IsGrounded())
+        {
+            rb.AddForce(movement * moveSpeed);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsGrounded())
+        {
+            rb.linearDamping = groundDrag;
+        }
+        else {
+            rb.linearDamping = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, groundDistance, GroundMask);
+    }
+}
