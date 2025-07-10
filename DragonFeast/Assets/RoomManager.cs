88e2c5ad0199c361dc2c2b825b91ac402daf3145
M00St4r <64206620+M00St4r@users.noreply.github.com>
2025-07-10 13:41:36 +0200

    add door andeler

--- DragonFeast/Assets/RoomManager.cs
+++ DragonFeast/Assets/RoomManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
+    // all rooms have to be children of this object
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,18 @@ public class RoomManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Transform[] childs = transform.GetComponentsInChildren<Transform>();
+
+        //iterate through all rooms
+        for (int i = 0; i < childs.Length;)
+        {
+            //check if all enemies got defeated
+            if (childs[i].transform.Find("Spawns").childCount == 0)
+            {
+                //destroy the door
+                Destroy(childs[i].transform.Find("door"));
+                i++;
+            }
+        }
     }
 }
