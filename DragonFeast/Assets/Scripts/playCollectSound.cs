0c4c4e10e59777a7ad65bb4195224de214f4734d
M00St4r <64206620+M00St4r@users.noreply.github.com>
2025-07-14 12:04:38 +0200

    add sound scripts

---
+++ DragonFeast/Assets/Scripts/playCollectSound.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class playCollectSound : MonoBehaviour
+{
+    [SerializeField] FMODUnity.EventReference CollectEggAud;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playCollect()
+    {
+        print("collected an egg");
+        FMODUnity.RuntimeManager.PlayOneShot(CollectEggAud);
+    }
+}
